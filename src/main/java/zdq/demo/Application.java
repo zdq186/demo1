6f60d29347f96c8e25984e5306dea3fb96c5465e
zdq <zdq186@126.com>
2017-04-06 23:19:42 +0800

    2121

--- src/main/java/zdq/demo/Application.java
+++ src/main/java/zdq/demo/Application.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class Application {
     @RequestMapping("/")
     String home() {
+        System.out.println("hello world 33333333333");
         return "Hello World!33333";
     }
     public static void main(String[] args) throws Exception {
